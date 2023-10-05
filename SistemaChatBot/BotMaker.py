64f241b3f4a7ab71e59bbc1a89e220c62aa70be8
Pedro Gimenez <pehqge@gmail.com>
2023-10-05 18:51:05 -0300

    novas mudancas

--- SistemaChatBot/BotMaker.py
+++ SistemaChatBot/BotMaker.py
@@ -5,23 +5,19 @@ from SistemaChatBot import SistemaChatBot as scb
 class BotMaker:
     def __init__(self, sistema: scb):
         self.__sistema = sistema
-        self.__nome = None
         self.__perguntas_respostas = []
-        self.__apresentacao = None
-        self.__boas_vindas = None
-        self.__despedida = None
     
     @property
     def sistema(self):
         return self.__sistema
         
-    def add_bot(self):
+    def add_bot(self, nome, apresentacao, boas_vindas, despedida):
         if self.__nome in [bot.nome for bot in self.sistema.lista_bots]:
             raise ValueError("Já existe um bot com esse nome")
-        elif self.__nome == None or self.__apresentacao == None or self.__boas_vindas == None or self.__despedida == None or self.__perguntas_respostas == []:
+        elif nome == "" or apresentacao == "" or boas_vindas == "" or despedida == "" or self.__perguntas_respostas == []:
             raise ValueError("Por favor, preencha todos os dados do bot")
         else:
-            self.sistema.lista_bots.append(Bot(self.__nome, self.__apresentacao, self.__boas_vindas, self.__despedida, self.__perguntas_respostas))
+            self.sistema.lista_bots.append(Bot(nome, apresentacao, boas_vindas, despedida, self.__perguntas_respostas))
     
     def remove_bot(self, bot: Bot):
         if bot in self.sistema.lista_bots:
--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -3,7 +3,7 @@ from Bots.Bot import Bot
 class SistemaChatBot:
     def __init__(self, nomeEmpresa, lista_bots):
         self.__empresa = nomeEmpresa
-        self.__lista_bots= lista_bots
+        self.__lista_bots = lista_bots
         if not all(isinstance(bot, Bot) for bot in self.__lista_bots):
             raise ValueError("A lista deve conter apenas Bots.")
         self.__bot = None
